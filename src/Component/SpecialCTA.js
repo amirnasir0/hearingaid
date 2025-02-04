dc5d6955715a5dafbb9bea2f092b65a381e9b447
amirnasir0 <amirnasir0@gmail.com>
2025-02-05 00:07:20 +0530

    Update SpecialCTA.js

--- src/Component/SpecialCTA.js
+++ src/Component/SpecialCTA.js
@@ -1,25 +1,7 @@
 // import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const SpecialCTA = () => {
-//   const navigate = useNavigate();
-//   return (
-//     <div className="max-w-lg mx-auto m-7 flex items-center justify-center relative">
-//       {/* CTA Button */}
-//       <button
-//         onClick={() => navigate("/form")}
-//         className="px-6 py-3 bg-orange-500 text-white text-lg rounded-lg shadow-lg hover:bg-orange-600 transition"
-//       >
-//         Download Latest Price List
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SpecialCTA;
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import DetailsForm from "../Component/TopForm/detailsForm.js";
+import DetailsForm from "../Component/TopForm/PriceForm.js";
 
 const SpecialCTA = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -61,7 +43,7 @@ const SpecialCTA = () => {
               </div>
             ) : (
               <>
-                <DetailsForm onSubmit={handleSubmit} />
+                <PriceForm onSubmit={handleSubmit} />
               </>
             )}
           </div>
