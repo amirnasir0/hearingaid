97e648af6aeba027f70aacdfc57298e318c31388
amirnasir0 <amirnasir0@gmail.com>
2025-02-05 14:33:46 +0530

    Update SpecialCTA.js

--- src/Component/SpecialCTA.js
+++ src/Component/SpecialCTA.js
@@ -1,7 +1,7 @@
 // import React from "react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import DetailsForm from "../Component/TopForm/PriceForm.js";
+import PriceForm from "../Component/TopForm/PriceForm.js";
 
 const SpecialCTA = () => {
   const [showPopup, setShowPopup] = useState(false);
