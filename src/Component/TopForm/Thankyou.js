1eebcbaea547f06079816c408ad850b1d42ad856
Shubham Kumar <112254580+Shubhamkumar8321@users.noreply.github.com>
2025-02-19 18:59:45 +0530

    Update Thankyou.js

--- src/Component/TopForm/Thankyou.js
+++ src/Component/TopForm/Thankyou.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const ThankYouPage = ({ name = "Guest" }) => {
   return (
-    // <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-      <div className="bg-white p-6 md:p-8 rounded-lg  max-w-lg w-full text-center">
-        <h2 className="text-2xl md:text-3xl font-bold text-gray-700">
-          🎉 Congratulations, {name}! <br />
-          You’ve Unlocked an Exclusive Discount!
+    <div className="flex items-center justify-center ">
+      <div className="bg-white rounded-lg  max-w-lg w-full text-center">
+        <h2 className="text-3xl md:text-3xl font-bold text-gray-700">
+          🎉Congratulations,{name}!
+          <p className="text-lg md:text-xl font-semibold text-gray-700">You’ve Unlocked an Exclusive Discount!</p> 
         </h2>
         <p className="text-gray-500 mt-4 text-sm md:text-base">
-          Your special offer is now active! But hurry—this extra discount is valid only for the next <strong>10 minutes</strong>! ⏳
-        📞 Call now to claim your offer before it expires!
+          Your special offer is now active! But hurry—this extra discount is valid only for the next <strong>10 minutes</strong>! ⏳<br/>📞
+          Call now to claim your offer before it expires!
         </p>
 
         {/* Buttons */}
@@ -23,7 +23,7 @@ const ThankYouPage = ({ name = "Guest" }) => {
           </a>
           <a
             href="https://api.whatsapp.com/send/?phone=917742573686&text=hi&type=phone_number&app_absent=0"
-            className="bg-orange-500 text-white py-3 px-6 rounded-lg font-semibold w-full md:w-auto text-center hover:bg-orange-600 transition duration-300"
+            className="bg-green-500 text-white py-3 px-6 rounded-lg font-semibold w-full md:w-auto text-center hover:bg-green-600 transition duration-300"
           >
            WhatsApp Now
           </a>
@@ -40,7 +40,7 @@ const ThankYouPage = ({ name = "Guest" }) => {
           </a>.
         </p>
       </div>
-    // </div>
+     </div>
   );
 };
 
